c01a8e6bdcb31a6a39fd9232dff47e5ef8313ef6
alemoles <alemolerio@gmail.com>
2023-02-28 20:12:27 -0300

    Kruskal's Algorithm | Union-Find

--- graph-algorithms/src/main/java/com/devstromo/kruskal/DisjointSet.java
+++ graph-algorithms/src/main/java/com/devstromo/kruskal/DisjointSet.java
@@ -1,4 +1,63 @@
 package com.devstromo.kruskal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DisjointSet {
+
+    public DisjointSet(List<Vertex> vertexList) {
+        makeSets(vertexList);
+    }
+
+    // find the representative (root node) for node 'node'
+    public Node find(Node node) {
+        var actual = node;
+        // find the representative
+        while (actual.getParent() != null) {
+            actual = actual.getParent();
+        }
+
+        // "path compression" to make sure that next time we look for
+        // the representative of the node we have O(1)
+        Node root = actual;
+        actual = node;
+
+        while (actual != root) {
+            var temp = actual.getParent();
+            actual.setParent(root);
+            actual = temp;
+        }
+        return root;
+    }
+
+    // combine two sets
+    public void union(Node node1, Node node2) {
+        var root1 = find(node1);
+        var root2 = find(node2);
+        if (root1 == root2) {
+            return;
+        }
+
+        // attach the smaller tree to the root of the larger tree "union by height"
+        if (root1.getHeight() < root2.getHeight()) {
+            root1.setParent(root2);
+        } else if (root1.getHeight() > root2.getHeight()) {
+            root2.setParent(root1);
+        } else {
+            root2.setParent(root1);
+            root1.setHeight(root1.getHeight() + 1);
+        }
+
+    }
+
+    private void makeSets(List<Vertex> vertexList) {
+        for (var vertex : vertexList) {
+            makeSet(vertex);
+        }
+    }
+
+    private void makeSet(Vertex vertex) {
+        var node = new Node(0, null);
+        vertex.setNode(node);
+    }
 }
--- graph-algorithms/src/main/java/com/devstromo/kruskal/Node.java
+++ graph-algorithms/src/main/java/com/devstromo/kruskal/Node.java
@@ -1,24 +1,15 @@
 package com.devstromo.kruskal;
 
 public class Node {
-    // a unique index for each node in the tree
-    private int id;
+
     // the approximate count of the nodes below this node
     private int height;
     private Node parent;
 
-    public Node(int id, int height, Node parent) {
-        this.id = id;
+    public Node(int height, Node parent) {
+
         this.height = height;
         this.parent = parent;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public int getHeight() {
