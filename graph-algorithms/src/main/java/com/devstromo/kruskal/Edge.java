ebaeb775191c87713fd3376a3ead71978045d70a
alemoles <alemolerio@gmail.com>
2023-02-28 20:23:54 -0300

    Kruskal's Algorithm

--- graph-algorithms/src/main/java/com/devstromo/kruskal/Edge.java
+++ graph-algorithms/src/main/java/com/devstromo/kruskal/Edge.java
@@ -1,6 +1,6 @@
 package com.devstromo.kruskal;
 
-public record Edge(double weight, Vertex start, Vertex target) implements Comparable<Edge> {
+public record Edge(Vertex start, Vertex target, double weight) implements Comparable<Edge> {
     @Override
     public int compareTo(Edge other) {
         // compare edge based on the weight
--- graph-algorithms/src/main/java/com/devstromo/kruskal/KruskalAlgorithm.java
+++ graph-algorithms/src/main/java/com/devstromo/kruskal/KruskalAlgorithm.java
@@ -1,4 +1,30 @@
 package com.devstromo.kruskal;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class KruskalAlgorithm {
+
+    public void run(List<Vertex> vertexList, List<Edge> edgeList) {
+        var disjointSet = new DisjointSet(vertexList);
+        var mst = new ArrayList<Edge>();
+        // use merge sort to sort the edges
+        Collections.sort(edgeList);
+        for (var edge : edgeList) {
+            var u = edge.start();
+            var v = edge.target();
+            // the edge is in the MST is the nodes are in different sets
+            if (disjointSet.find(u.getNode()) != disjointSet.find(v.getNode())) {
+                mst.add(edge);
+            }
+            //we have to merge the sets
+            disjointSet.union(u.getNode(), v.getNode());
+
+        }
+        // show mst
+        for (var edge : mst) {
+            System.out.println(edge);
+        }
+    }
 }
