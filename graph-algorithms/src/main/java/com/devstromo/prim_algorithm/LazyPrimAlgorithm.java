2234729af88b8ef2446796cc662f1792547b3445
alemoles <alemolerio@gmail.com>
2023-03-01 21:17:26 -0300

    Lazy Prim's Algorithm

---
+++ graph-algorithms/src/main/java/com/devstromo/prim_algorithm/LazyPrimAlgorithm.java
@@ -0,0 +1,62 @@
+package com.devstromo.prim_algorithm;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+public class LazyPrimAlgorithm {
+
+    // most of the operations have O(1) running time
+    private final Set<Vertex> unvisited;
+
+    private final List<Edge> mst;
+
+    private final PriorityQueue<Edge> heap;
+    private double fullCost;
+
+    public LazyPrimAlgorithm(List<Vertex> unvisited) {
+        this.unvisited = new HashSet<>(unvisited);
+        this.mst = new ArrayList<>();
+        this.heap = new PriorityQueue<>();
+    }
+
+    public void run(Vertex vertex) {
+        // remove the start vertex (source)
+        unvisited.remove(vertex);
+
+        // the algorithm terminates when we visit all the vertices
+        while (!unvisited.isEmpty()) {
+            // insert all the edges into the heap except for the
+            // edges leading to already visited vertices
+            for (var edge : vertex.getAdjacencyList()) {
+                if (unvisited.contains(edge.target())) {
+                    heap.add(edge);
+                }
+            }
+            // get the minimum edge from the head in O(logE)
+            var minEdge = heap.remove();
+            // get another edge if the edge is leading to
+            // an already visited vertex
+            if (!unvisited.contains(minEdge.target())) {
+                continue;
+            }
+
+            mst.add(minEdge);
+            fullCost += minEdge.weight();
+
+            // in the next iteration we consider the next vertex
+            vertex = minEdge.target();
+            unvisited.remove(vertex);
+        }
+
+    }
+
+    public void printSolution() {
+        System.out.println("Cost of MST: " + this.fullCost);
+        for (var edge : this.mst) {
+            System.out.println(edge.start() + "-" + edge.target());
+        }
+    }
+}
---
+++ graph-algorithms/src/main/java/com/devstromo/prim_algorithm/Vertex.java
@@ -0,0 +1,41 @@
+package com.devstromo.prim_algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vertex {
+
+    private String name;
+
+    private final List<Edge> adjacencyList;
+
+    public Vertex(String name) {
+        this.name = name;
+        this.adjacencyList = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Edge> getAdjacencyList() {
+        return adjacencyList;
+    }
+
+    public void addNeighbor(Edge edge) {
+        this.adjacencyList.add(edge);
+    }
+
+    public void addNeighbor(double weight, Vertex target) {
+        this.adjacencyList.add(new Edge(this, target, weight));
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
