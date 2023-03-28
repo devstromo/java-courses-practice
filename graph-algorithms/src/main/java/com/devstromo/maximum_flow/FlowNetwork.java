28d0b59b5537c7349ac4c816c3abd7cc6b29c56c
alemoles <alemolerio@gmail.com>
2023-03-28 20:26:15 -0300

    Ford Fulkerson Algorithm | Maximum Flow

--- graph-algorithms/src/main/java/com/devstromo/maximum_flow/FlowNetwork.java
+++ graph-algorithms/src/main/java/com/devstromo/maximum_flow/FlowNetwork.java
@@ -1,4 +1,43 @@
 package com.devstromo.maximum_flow;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FlowNetwork {
+
+    private int numOfVertices;
+    private int numOfEdges;
+    private List<List<Edge>> adjacenciesList;
+
+    public FlowNetwork(int numOfVertices) {
+        this.numOfVertices = numOfVertices;
+        this.numOfEdges = 0;
+        this.adjacenciesList = new ArrayList<>();
+        for (int i = 0; i < numOfVertices; i++) {
+            var edges = new ArrayList<Edge>();
+            adjacenciesList.add(edges);
+        }
+    }
+
+    public int getNumOfVertices() {
+        return this.numOfVertices;
+    }
+
+    public int getNumOfEdges() {
+        return numOfEdges;
+    }
+
+    public void addEdge(Edge edge) {
+        var v = edge.getFrom();
+        var w = edge.getTarget();
+        adjacenciesList.get(v.getId())
+          .add(edge);
+        adjacenciesList.get(w.getId())
+          .add(edge);
+        numOfEdges++;
+    }
+
+    public List<Edge> getAdjacenciesList(Vertex vertex) {
+        return adjacenciesList.get(vertex.getId());
+    }
 }
