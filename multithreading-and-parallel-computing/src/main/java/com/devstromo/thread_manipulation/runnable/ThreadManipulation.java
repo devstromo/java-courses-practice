477706ac5d3f01acfb52ddc0f90b2a87374ff84c
alemoles <alemolerio@gmail.com>
2023-04-27 20:48:50 -0300

    Multi-thread Processing | Starting Threads

--- multithreading-and-parallel-computing/src/main/java/com/devstromo/thread_manipulation/runnable/ThreadManipulation.java
+++ multithreading-and-parallel-computing/src/main/java/com/devstromo/thread_manipulation/runnable/ThreadManipulation.java
@@ -6,20 +6,25 @@ public class ThreadManipulation {
         // IT'S NOT PARALLEL EXECUTION
         Thread t1 = new Thread(new Runner1());
         Thread t2 = new Thread(new Runner2());
-        Thread t3 = new Thread(() -> {
+        Thread t3 = new Runner3();
+        Thread t4 = new Runner4();
+        Thread t5 = new Thread(() -> {
             for (var i = 0; i < 10; i++) {
-                System.out.println("Runner3: " + i);
+                System.out.println("Runner5: " + i);
             }
         });
-        Thread t4 = new Thread(() -> {
+        Thread t6 = new Thread(() -> {
             for (var i = 0; i < 10; i++) {
-                System.out.println("Runner4: " + i);
+                System.out.println("Runner6: " + i);
             }
         });
         t1.start();
         t2.start();
         t3.start();
         t4.start();
+        t5.start();
+        t6.start();
+
     }
 }
 
@@ -38,6 +43,35 @@ class Runner1 implements Runnable {
     public void run() {
         for (var i = 0; i < 10; i++) {
             System.out.println("Runner1: " + i);
+        }
+    }
+}
+
+class Runner3 extends Thread {
+    @Override
+    public void run() {
+        for (var i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Runner3: " + i);
+        }
+    }
+}
+
+class Runner4 extends Thread {
+
+    @Override
+    public void run() {
+        for (var i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Runner4: " + i);
         }
     }
 }
