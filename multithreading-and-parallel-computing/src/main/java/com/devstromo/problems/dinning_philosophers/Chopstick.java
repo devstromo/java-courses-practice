7cdfad2881ca88603cff481c2576988555c25daa
alemoles <alemolerio@gmail.com>
2023-06-07 20:19:15 -0300

    Multi-thread Processing | Dining Philosophers Problem

--- multithreading-and-parallel-computing/src/main/java/com/devstromo/problems/dinning_philosophers/Chopstick.java
+++ multithreading-and-parallel-computing/src/main/java/com/devstromo/problems/dinning_philosophers/Chopstick.java
@@ -1,4 +1,35 @@
 package com.devstromo.problems.dinning_philosophers;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 class Chopstick {
+
+    private final int id;
+    private final Lock lock;
+
+    public Chopstick(int id) {
+        this.id = id;
+        this.lock = new ReentrantLock();
+    }
+
+    public boolean pickUp(Philosopher philosopher, State state) throws InterruptedException {
+        // simulate deadlock
+        if (lock.tryLock(10, TimeUnit.MILLISECONDS)) {
+            System.out.println(philosopher + " pick up " + state.toString() + " " + this);
+            return true;
+        }
+        return false;
+    }
+
+    public void putDown(Philosopher philosopher, State state) {
+        lock.unlock();
+        System.out.println(philosopher + " put down  " + state.toString() + " " + this);
+    }
+
+    @Override
+    public String toString() {
+        return "Chopstick " + id;
+    }
 }
