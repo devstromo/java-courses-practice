47a98fc480965dd017c4447ac5c94b9bba8fa875
alemoles <alemolerio@gmail.com>
2023-06-25 19:13:50 -0300

    Multi-thread Processing | Fork Join | Merge sort revisited

---
+++ multithreading-and-parallel-computing/src/main/java/com/devstromo/fork_join_framework/mergeSort/App.java
@@ -0,0 +1,33 @@
+package com.devstromo.fork_join_framework.mergeSort;
+
+import java.util.Random;
+import java.util.concurrent.ForkJoinPool;
+
+public class App {
+    public static void main(String[] args) {
+
+        int[] nums = initializeNums();
+        var mergesort = new SequentialMergeSort(nums);
+
+        long start = System.currentTimeMillis();
+        mergesort.mergeSort(nums);
+        System.out.println("Time taken with sequential sort: " + (System.currentTimeMillis() - start) + "ms");
+        try (var pool = new ForkJoinPool(Runtime.getRuntime()
+          .availableProcessors());) {
+            MergeSortTask rootTask = new MergeSortTask(nums);
+            start = System.currentTimeMillis();
+            pool.invoke(rootTask);
+            System.out.println("Time taken with parallel sort: " + (System.currentTimeMillis() - start) + "ms");
+        }
+
+    }
+
+    private static int[] initializeNums() {
+        Random random = new Random();
+        int[] nums = new int[100000000];
+        for (int i = 0; i < 100000000; ++i)
+            nums[i] = random.nextInt(100);
+
+        return nums;
+    }
+}
